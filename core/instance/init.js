3f296a3bdd4f341c0f42545faf6d96608e36491a
chenyu-max <56399206+chenyu-max@users.noreply.github.com>
2021-05-17 19:50:17 +0800

    18.生命周期-create

--- core/instance/init.js
+++ core/instance/init.js
@@ -8,11 +8,14 @@ export function initMixin(Due) {
         const vm = this;
         vm.uid = uid++;
         vm._isDue = true;
+        // 初始化数据 Data
         if (options && options.data) {
             vm._data = constructorProxy(vm, options.data, "");
         }
-        // 初始化数据 Data
         // 初始化 create 方法
+        if (options && options.created) {
+            vm._created = options.created;
+        }
         // 初始化 methods
         if (options && options.methods) {
             vm._methods = options.methods;
@@ -21,6 +24,9 @@ export function initMixin(Due) {
             }
         }
         // 初始化 computed
+        if (options && options.computed) {
+            vm._computed = options.computed;
+        }
         // 初始化 el 并挂载
         if (options && options.el) {
             let rootDom = document.getElementById(options.el);
